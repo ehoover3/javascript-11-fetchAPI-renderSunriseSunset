13a9e115c0adefed507370417a1249fcb1cd7f33
Eric Hoover <ehoover3@yahoo.com>
2022-02-03 12:58:32 -0800

    updated code

--- code.js
+++ code.js
@@ -1,25 +1,25 @@
 // SELECTORS
 let textDiv = document.querySelector("#text");
-let latitudeDiv = document.getElementById("latitude");
-let longitudeDiv = document.getElementById("longitude");
 
 // DATA
-let latitude;
-let longitude;
+let latitude = 5;
+let longitude = 20;
 
 // FUNCTION
-async function getSunriseAndSunset() {
-  // 1. update variables to form input values
-  latitude = latitudeDiv.value;
-  longitude = longitudeDiv.value;
+// 1. fetch data from API using the latitude and longitude values
+async function getSunriseSunset() {
+  // 1a. In the below https link, '?' indicates the start of a query search,
+  //     where the query searches the key value pairs of 'lat={$latitude}' and 'lng=${longitude}'
 
-  // 2. fetch data from API using the latitude and longitude values
+  // 1b. response will equal data from the API in a JSON format
   let response = await fetch(
     `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`
-  ); // response will equal data from the API in a JSON format
-  let data = await response.json(); // data will convert response data with the .json() method into usable data
+  );
 
-  // 3. set usable data to the textDiv's innerHTML
+  // 1c. data will convert response data with the .json() method into usable data
+  let data = await response.json();
+
+  // 2. set usable data to the textDiv's innerHTML
   textDiv.innerHTML = `
   <p>For latitude ${latitude} and longitude ${longitude}:</p>
   <p>Sunrise is at ${data.results.sunrise}</p>
@@ -27,7 +27,5 @@ async function getSunriseAndSunset() {
   `;
 }
 
-// EVENT LISTENERS
-btn.addEventListener("click", () => {
-  getSunriseAndSunset(); // make a new fetch call that updates img
-});
+// RUN THE FUNCTION
+getSunriseSunset();
